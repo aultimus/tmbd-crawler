81bfa0441280eaba7b598c94a1502eb016d349b2
Matthew Ault <matthew.ault@gmail.com>
2025-06-30 21:51:42 +0100

    add incremental crawl option

--- scripts/crawl.py
+++ scripts/crawl.py
@@ -1,13 +1,17 @@
 import argparse
 import asyncio
 import aiohttp
+import os
 from tqdm import tqdm
 from tmdb_crawler.fetcher import fetch_movie_and_credits
 from tmdb_crawler.storage import load_movie_ids, save_data
 from tmdb_crawler.config import API_KEY
 
 
-async def worker(movie_id, session, semaphore, output_dir):
+async def worker(movie_id, session, semaphore, output_dir, incremental):
+    path = os.path.join(output_dir, f"{movie_id}.json")
+    if incremental and os.path.exists(path):
+        return  # Skip if file exists
     async with semaphore:
         movie_data, credits_data = await fetch_movie_and_credits(
             session, API_KEY, movie_id
@@ -15,12 +19,15 @@ async def worker(movie_id, session, semaphore, output_dir):
         save_data(movie_id, movie_data, credits_data, output_dir=output_dir)
 
 
-async def main(movie_ids_file, concurrent_requests, output_dir):
+async def main(movie_ids_file, concurrent_requests, output_dir, incremental):
     movie_ids = load_movie_ids(movie_ids_file)
     semaphore = asyncio.Semaphore(concurrent_requests)
 
     async with aiohttp.ClientSession() as session:
-        tasks = [worker(mid, session, semaphore, output_dir) for mid in movie_ids]
+        tasks = [
+            worker(mid, session, semaphore, output_dir, incremental)
+            for mid in movie_ids
+        ]
 
         for f in tqdm(asyncio.as_completed(tasks), total=len(tasks)):
             await f
@@ -44,9 +51,21 @@ def parse_args():
         default="data",
         help="Directory to store output JSON files (default: data)",
     )
+    parser.add_argument(
+        "--incremental",
+        action="store_true",
+        help="Skip downloading if JSON file already exists (default: False)",
+    )
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
-    asyncio.run(main(args.movie_ids_file, args.concurrent_requests, args.output_dir))
+    asyncio.run(
+        main(
+            args.movie_ids_file,
+            args.concurrent_requests,
+            args.output_dir,
+            args.incremental,
+        )
+    )
